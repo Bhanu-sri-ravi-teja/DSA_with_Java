97b691e34e491862f8ba1eb0bad944329421513b
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-06-10 19:51:41 +0530

    Time: 4 ms (75.41%) | Memory: 45 MB (77.05%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        for (int i = 0; i < nums1.length; i++) {
-            boolean found = false;
-
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == nums1[i]) {
-
-                    for (int k = j + 1; k < nums2.length; k++) {
-                        if (nums2[k] > nums1[i]) {
-                            nums1[i] = nums2[k];
-                            found = true;
+        for(int i=0; i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
+                if(nums1[i] == nums2[j]){
+                    
+                    while(j<nums2.length){
+                        j++;
+                        if(j >= nums2.length){
+                            nums1[i] = -1;
+                            j = nums2.length;
+                            break;
+                        }else if(nums2[j] > nums1[i]){
+                            nums1[i] = nums2[j];
+                            j = nums2.length;
                             break;
                         }
                     }
-
-                    if (!found) {
-                        nums1[i] = -1;
-                    }
-                    break;
                 }
             }
         }
-
         return nums1;
     }
 }
