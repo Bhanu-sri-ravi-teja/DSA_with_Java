e0a559fe0b7c1748e657419e02f2bc258070cc36
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-08-20 13:55:22 +0530

    Time: 13 ms (98.18%), Space: 108.8 MB (17.63%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,8 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer,Integer> seq = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
         for(int i : nums){
-            int val = seq.getOrDefault(i,0)+1;
-            seq.put(i,val);
-        }
-        for(int i : seq.values()){
-            if(i>1){
+            if(!set.add(i)){
                 return true;
             }
         }
