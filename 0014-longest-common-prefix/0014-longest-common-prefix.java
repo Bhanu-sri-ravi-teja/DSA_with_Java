c902693c83381651ff8eb616fac7bd255ed48edd
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-08-20 17:36:42 +0530

    Time: 0 ms (100%), Space: 43.5 MB (7.62%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String temp = strs[0];
+        for(int i=1; i<strs.length; i++){
+            while(strs[i].indexOf(temp) != 0){
+                temp = temp.substring(0,temp.length() - 1);
+            }
+        }
+        return temp;
+    }
+}
