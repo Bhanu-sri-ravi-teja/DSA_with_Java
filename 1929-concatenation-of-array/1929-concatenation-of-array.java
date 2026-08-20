ed48bc66fb422b6d68cdb382c3b24e420028428b
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-08-20 13:43:55 +0530

    Time: 1 ms (97.74%), Space: 47.1 MB (77.18%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -2,12 +2,9 @@ class Solution {
     public int[] getConcatenation(int[] nums) {
         int n = nums.length;
         int[] arr = new int[n*2];
-        for(int i=0; i<n*2; i++){
-            if(i<n){
-                arr[i] = nums[i];
-            }else{
-                arr[i] = nums[i-n];
-            }
+        for(int i=0; i<n; i++){
+            arr[i] = nums[i];
+            arr[i+n] = nums[i];
         }
         return arr;
     }
