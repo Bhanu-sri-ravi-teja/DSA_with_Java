bdbef28c1da1271e353b769343c58683efc69d23
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-08-21 10:06:16 +0530

    Time: 12 ms (37.46%) | Memory: 52.7 MB (80.61%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,20 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate = 0;
-        int count = 0;
-
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
-            }
-
-            if (num == candidate) {
-                count++;
-            } else {
-                count--;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num:nums){
+            int seq = map.getOrDefault(num,0);
+            seq++;
+            map.put(num,seq);
+        }
+        int highestElement = 0;
+        int highestValue = 0;
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            int key = entry.getKey();
+            int value = entry.getValue();
+            if(value > highestValue){
+                highestValue = value;
+                highestElement = key;
+                
             }
         }
-
-        return candidate;
+        return highestElement;
     }
 }
