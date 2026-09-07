002910f4660ed1497225508ed03800ba4ec837c8
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-09-07 10:48:21 +0530

    Time: 1 ms (99.34%) | Memory: 45.9 MB (96.00%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -1,23 +1,26 @@
 class Solution {
-    int Min(int i, int[] nums,int n){
-        int min = Integer.MAX_VALUE;
-        for(int j=i; j<n; j++){
-            min = Math.min(min,nums[j]);
-        }
-        return min;
-    }
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
-        int min;
-        int max = 0;
-        int res = Integer.MAX_VALUE;
-        for(int i=0; i<n; i++){
-            max = Math.max(max,nums[i]);
-            min = Min(i,nums,n);
-            if(max-min<=k){
-                res = Math.min(res,i);
+
+        int[] suffixMin = new int[n];
+        suffixMin[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, nums[i]);
+
+            int score = max - suffixMin[i];
+
+            if (score <= k) {
+                return i;
             }
         }
-        return res == Integer.MAX_VALUE? -1:res;
+
+        return -1;
     }
 }
