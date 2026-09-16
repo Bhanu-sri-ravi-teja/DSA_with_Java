66e353c4e249e2892602fc323e36626881152b8d
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-09-16 15:47:49 +0530

    Time: 1 ms (0.43%) | Memory: 42.7 MB (85.82%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        System.out.println(head.val);
+        Boolean bool = true;
+        int count = 0;
+        ListNode temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+            if(bool == true){
+                head = head.next;
+            }
+            bool = !bool;
+        }
+        return head;
+    }
+}
