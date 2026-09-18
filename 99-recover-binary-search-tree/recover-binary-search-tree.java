defbca943b432318f0bfc3729d182978620db641
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-09-18 21:38:47 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (49.39%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private TreeNode prev;
+    private TreeNode swapNumb;
+    private TreeNode firstMax;
+    private TreeNode secondMax;
+
+    private void check(TreeNode root){
+        if(root == null)
+            return;
+        check(root.left);
+        if(prev != null && (prev.val > root.val)){
+            if(swapNumb == null){
+                swapNumb = prev;
+                firstMax = root;
+            }else 
+                secondMax = root;
+        }
+        prev = root;
+        check(root.right);
+    }
+
+    public void recoverTree(TreeNode root) {
+        check(root);
+        if(secondMax == null && swapNumb != null){
+            int temp = swapNumb.val;
+            swapNumb.val = firstMax.val;
+            firstMax.val = temp;
+        }else if((secondMax != null) && (swapNumb != null)){
+            int temp = swapNumb.val;
+            swapNumb.val = secondMax.val;
+            secondMax.val = temp;
+        }
+    }
+}
