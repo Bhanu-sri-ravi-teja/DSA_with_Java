bd40d12d1a85f1bbfae88f69166d0e6ddb6eacf1
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-09-20 00:33:48 +0530

    Time: 4 ms (87.56%) | Memory: 48.3 MB (56.34%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.java
@@ -1,23 +1,19 @@
 class Solution {
     public int[] rowAndMaximumOnes(int[][] mat) {
-        int max = 0;
-        int row = 0;
-
-        for (int i = 0; i < mat.length; i++) {
-            int count = 0;
-
-            for (int j = 0; j < mat[i].length; j++) {
-                if (mat[i][j] == 1) {
-                    count++;
+        int maxOnesRow = 0;
+        int maxOnes = 0;
+        for(int i=0; i<mat.length; i++){
+            int rowOnesCount = 0;
+            for(int j=0; j<mat[0].length; j++){
+                if(mat[i][j] == 1){
+                    rowOnesCount++;
                 }
             }
-
-            if (count > max) {
-                max = count;
-                row = i;
+            if(maxOnes<rowOnesCount){
+                maxOnes = rowOnesCount;
+                maxOnesRow = i;
             }
         }
-
-        return new int[]{row, max};
+        return new int[]{maxOnesRow,maxOnes};
     }
 }
