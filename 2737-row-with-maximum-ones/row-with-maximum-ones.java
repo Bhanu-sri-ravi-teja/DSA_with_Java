ab41a33f23098850eef4b3521a4806bafbcf421a
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-09-20 00:30:50 +0530

    Time: 4 ms (87.56%) | Memory: 48.5 MB (20.78%) - LeetSync

---
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        int maxOnesRow = -1;
+        int maxOnes = 0;
+        for(int i=0; i<mat.length; i++){
+            int rowOnesCount = 0;
+            for(int j=0; j<mat[0].length; j++){
+                if(mat[i][j] == 1){
+                    rowOnesCount++;
+                }
+            }
+            if(maxOnes<rowOnesCount){
+                maxOnes = rowOnesCount;
+                maxOnesRow = i;
+            }
+        }
+        if(maxOnesRow== -1) return new int[]{0,maxOnes};        
+        return new int[]{maxOnesRow,maxOnes};
+    }
+}
