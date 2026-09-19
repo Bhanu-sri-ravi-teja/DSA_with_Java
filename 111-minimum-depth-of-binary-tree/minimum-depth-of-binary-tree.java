2de34b3657875953f30f4716419d48fb11a217f1
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-09-19 23:17:29 +0530

    Time: 5 ms (29.30%) | Memory: 82 MB (56.40%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int depth(TreeNode root, int depth){
+        if(root == null) return Integer.MAX_VALUE;
+        depth++;
+        if(root.left == null && root.right == null){
+            return depth;
+        }
+        return Math.min(
+            depth(root.left, depth),
+            depth(root.right,depth)
+        );
+    }
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        return depth(root, 0);
+    }
+}
