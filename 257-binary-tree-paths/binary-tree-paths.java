dadc703270c193aed514294ce0ef03670990690d
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-09-19 22:27:42 +0530

    Time: 1 ms (99.86%) | Memory: 44 MB (77.91%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -1,32 +1,33 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    private List<String> list = new ArrayList<>();
-    private void getPath(TreeNode root,String path){
-        if(root == null) return;
-        path = path + root.val;
-        if(root.left == null && root.right == null){
-            list.add(path);
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ans = new ArrayList<>();
+        StringBuilder path = new StringBuilder();
+
+        dfs(root, path, ans);
+
+        return ans;
+    }
+
+    private void dfs(TreeNode root, StringBuilder path, List<String> ans) {
+        if (root == null) {
             return;
         }
-        getPath(root.left, path + "->");
-        getPath(root.right, path + "->");
-    }
-    public List<String> binaryTreePaths(TreeNode root) {
-        getPath(root, "");
-        return list;
+
+        int len = path.length();
+
+        if (len > 0) {
+            path.append("->");
+        }
+
+        path.append(root.val);
+
+        if (root.left == null && root.right == null) {
+            ans.add(path.toString());
+        } else {
+            dfs(root.left, path, ans);
+            dfs(root.right, path, ans);
+        }
+
+        path.setLength(len);
     }
 }
