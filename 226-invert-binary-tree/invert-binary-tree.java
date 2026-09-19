2dffe50e6fe27a266413899d3572c0b992ff9ff3
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-09-19 22:49:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (27.28%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -14,16 +14,13 @@
  * }
  */
 class Solution {
-    void invert(TreeNode root){
-        if(root == null) return;
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
-        invert(root.left);
-        invert(root.right);
-    }
-    public TreeNode invertTree(TreeNode root) {
-        invert(root);
+        invertTree(root.left);
+        invertTree(root.right);
         return root;
     }
 }
